ca3057f14dda3ac1418a7ff5752b6f8e1c9c4872
vinay432 <54946859+vinay432@users.noreply.github.com>
2020-05-14 19:58:35 +0530

    Add files via upload

---
+++ inputs from user.py
@@ -0,0 +1,8 @@
+#Taking inputs from user,we need to explain the user what type of input to be accepted by code like integer or float, it may be string type.
+
+a=input("Enter your lucky number:") #a is a varable to store inputs which is given by user
+print(a) #printing the lucky number out.
+#if you wanna specify particularly, user can enter only strings then
+a=str(input("enter your text here to dispaly:"))
+print(a) #printing out the textZ
+
